161cb2bfbf660074d57694fca243d8a0b00c7727
Roba <roba@vrstu.net>
2022-04-26 00:19:31 +0900

    Request に headers を追加

--- sample_program/scrapysample/urllibsample/sample_05.py
+++ sample_program/scrapysample/urllibsample/sample_05.py
@@ -4,16 +4,16 @@ import urllib.request
 url = 'https://www.baidu.com/s?wd='
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKi'
-                  't/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/5'
-                  '37.36'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/'
+                  '537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.'
+                  '36'
 }
 
 # 「日本語」の三文字をunicodeに変換
 name = urllib.parse.quote('日本語')
 url += name
 
-request = urllib.request.Request(url=url)
+request = urllib.request.Request(url=url, headers=headers)
 
 response = urllib.request.urlopen(request)
 
