8d8000bcc0cf2a86e887de467a703b2145509eaa
Roba <roba@vrstu.net>
2022-04-25 23:36:29 +0900

    quote の使い方

---
+++ sample_program/scrapysample/urllibsample/sample_05.py
@@ -0,0 +1,22 @@
+import urllib.parse
+import urllib.request
+
+url = 'https://www.baidu.com/s?wd='
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKi'
+                  't/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/5'
+                  '37.36'
+}
+
+# 「日本語」の三文字をunicodeに変換
+name = urllib.parse.quote('日本語')
+url += name
+
+request = urllib.request.Request(url=url)
+
+response = urllib.request.urlopen(request)
+
+content = response.read().decode('utf-8')
+
+print(content)
