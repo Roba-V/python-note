7d1e00377b5767448770ffedf85ff1faee0cb7bd
Roba <roba@vrstu.net>
2022-04-26 19:55:51 +0900

    urllib Handler の基本的な使い方

---
+++ sample_program/scrapysample/urllibsample/sample_14.py
@@ -0,0 +1,26 @@
+# Handler で Baidu にアクセス
+
+import urllib.request
+
+url = 'http://www.baidu.com'
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWeb'
+                  'Kit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Saf'
+                  'ari/537.36'
+}
+
+request = urllib.request.Request(url=url, headers=headers)
+
+# Handler オブジェクトを取得
+handler = urllib.request.HTTPHandler()
+
+# Opener オブジェクトを取得
+opener = urllib.request.build_opener(handler)
+
+# open メソッドを呼び出し
+response = opener.open(request)
+
+content = response.read().decode('utf-8')
+
+print(content)
