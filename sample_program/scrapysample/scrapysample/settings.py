4d67a5c078b75e6d982eae1fc021e5f1a6a78bf6
Roba <roba@vrstu.net>
2022-04-28 01:03:24 +0900

    scrapy 複数のデータを逐次にダウンロード

--- sample_program/scrapysample/scrapysample/settings.py
+++ sample_program/scrapysample/scrapysample/settings.py
@@ -61,9 +61,9 @@ NEWSPIDER_MODULE = 'scrapysample.spiders'
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'scrapysample.pipelines.ScrapysamplePipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'scrapysample.pipelines.ScrapysamplePipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
---
+++ sample_program/scrapysample/scrapysample/spiders/spider3.py
@@ -0,0 +1,29 @@
+import scrapy
+
+from scrapysample.items import ScrapysampleItem
+
+
+class Spider3Spider(scrapy.Spider):
+    name = 'spider3'
+    allowed_domains = ['category.dangdang.com']
+    start_urls = ['https://category.dangdang.com/cp01.01.00.00.00.00.html']
+
+    def parse(self, response):
+        # piplines  データをダウンロードする
+        # items     データ構造を定義
+        li_list = response.xpath('//ul[@id="component_59"]/li')
+
+        print('=' * 100)
+
+        for li in li_list:
+            src = li.xpath('.//img/@data-original').extract_first()
+            if not src:
+                src = li.xpath('.//img/@src').extract_first()
+            name = li.xpath('.//img/@alt').extract_first()
+            price = li.xpath(
+                './/p[@class="price"]/span[1]/text()').extract_first()
+            print(src, name, price)
+
+            book = ScrapysampleItem(src=src, name=name, price=price)
+
+            yield book
