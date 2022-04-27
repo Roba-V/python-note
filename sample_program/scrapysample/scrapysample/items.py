fb11689ae981c5f3b0eabb59a846af2571a1d192
Roba <roba@vrstu.net>
2022-04-28 01:26:05 +0900

    scrapy 複数パイプラインの利用（コミット漏れの分）

--- sample_program/scrapysample/scrapysample/items.py
+++ sample_program/scrapysample/scrapysample/items.py
@@ -9,4 +9,10 @@ import scrapy
 class ScrapysampleItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    # 画像
+    src = scrapy.Field()
+    # 書名
+    name = scrapy.Field()
+    # 価格
+    price = scrapy.Field()
--- sample_program/scrapysample/scrapysample/pipelines.py
+++ sample_program/scrapysample/scrapysample/pipelines.py
@@ -5,9 +5,31 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import urllib.request
 
 
 class ScrapysamplePipeline:
+
+    def open_spider(self, spider):
+        print('+' * 100)
+        self.fp = open('book.json', 'a', encoding='utf-8')
+
     def process_item(self, item, spider):
+        self.fp.write(str(item))
+
+        return item
+
+    def close_spider(self, spider):
+        print('-' * 100)
+        self.fp.close()
+
+
+class DownloadImgPipeline:
+
+    def process_item(self, item, spider):
+        url = 'http:' + item.get('src')
+        filename = './books' + item.get('name') + '.jpg'
+
+        urllib.request.urlretrieve(url=url, filename=filename)
+
         return item
